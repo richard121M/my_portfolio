e5b77fd6691cf06577380135aa257084ba8a368c
richard121M <richardtavares520@gmail.com>
2025-04-18 21:41:24 -0300

    site v0.05

--- logic.js
+++ logic.js
@@ -1,31 +1,60 @@
-var buton_sobre = document.getElementById("sobre_b")
+let buton_sobre = document.getElementById("sobre_b");
+var elemente_load = document.getElementById("sobre_b");
 
 
+const info_instance = document.querySelector('.info');
+
+const myObserve = new IntersectionObserver((entries)=>{
+    alert("ddd")
+})
+
+myObserve.observe(info_instance)
+
+function test(){
+    alert("asasas")
+}
 function clicke(namo){
+    // setInterval(() => {
+    //     alert("sdasfaaaaaaaaaiiii mei")
+    //   }, 5000);
+    
     // alert("dddd")
     var sobre_screen = document.getElementById("sobre_s")
     var draw_screen = document.getElementById("draw_s")
     var prog_screen = document.getElementById("prog_s")
+
+    // prog_screena.id = "ssssssasa"
 
     sobre_screen.style.display = 'none';
     draw_screen.style.display = 'none';
     prog_screen.style.display = 'none';
-
+    // elemente_load(namo)
+    // ona(namo)
     if(document.getElementById(namo).style.display === 'block'){
         document.getElementById(namo).style.display = 'none';
     }else{
-        document.getElementById(namo).style.display = 'block';
+        setTimeout(() => {
+            document.getElementById(namo).style.display = 'block';
+        }, 100);
     }
 }
 function mostrar_tudo(){
-    // alert("dddd")
     var sobre_screen = document.getElementById("sobre_s")
     var draw_screen = document.getElementById("draw_s")
     var prog_screen = document.getElementById("prog_s")
-
-    sobre_screen.style.display = 'block';
-    draw_screen.style.display = 'block';
-    prog_screen.style.display = 'block';
+    
+    sobre_screen.style.display = 'none';
+    draw_screen.style.display = 'none';
+    prog_screen.style.display = 'none';
+    
+    setTimeout(() => {
+        sobre_screen.style.display = 'block';
+        draw_screen.style.display = 'block';
+        prog_screen.style.display = 'block';
+        
+        
+    }, 100); //1000
+    
 }
 
 function mexer_disco(){
